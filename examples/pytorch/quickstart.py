2a87ad2c29ae43b9bfc114b6fe0630c105b8020c
Your Name <you@example.com>
2025-06-05 21:46:45 +0000

    Merge master into aspctu/hybrid

--- examples/pytorch/quickstart.py
+++ examples/pytorch/quickstart.py
@@ -1,18 +1,26 @@
 from tensorrt_llm import SamplingParams
 from tensorrt_llm._torch import LLM
-from tensorrt_llm.llmapi import (EagleDecodingConfig, NGramDecodingConfig, HybridDecodingConfig, KvCacheConfig)
+from tensorrt_llm.llmapi import (EagleDecodingConfig, NGramDecodingConfig, KvCacheConfig) # HybridDecodingConfig
+from transformers import AutoTokenizer
+
+MODEL_NAME = "nvidia/Llama-3.3-70B-Instruct-FP8" # "meta-llama/Meta-Llama-3-8B-Instruct"
+EAGLE_PATH = "/workspace/eagle-llama3-70b" # "workspace/eagle"
 
 def main():
-    prompts = [
-        "Hello, my name is",
-    ]
-    sampling_params = SamplingParams(max_tokens=32)
+    # with open("code.txt", "r") as f:
+    #    prompts = f.read()
+    # prompts = [f"Here's some code : {prompts}\n What does the HybridDecodingConfig take in as arguments?"] 
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    prompts = ["Hi, my name is"]
+
+    sampling_params = SamplingParams(max_tokens=1028, end_id=128001)
 
     eagle_config = EagleDecodingConfig(
-        max_draft_len=2,
-        pytorch_eagle_weights_path="/workspace/eagle"
+        max_draft_len=32,
+        pytorch_eagle_weights_path=EAGLE_PATH,
     )
-    
+
     ngram_config = NGramDecodingConfig(
         prompt_lookup_num_tokens=32,
         max_matching_ngram_size=32,
@@ -21,24 +29,21 @@ def main():
         is_public_pool=True,
     )
 
-    spec_config = HybridDecodingConfig(
-        eagle_config=eagle_config,
-        ngram_config=ngram_config,
-        max_eagle_potential_drafts=2,
-        max_ngram_potential_drafts=32,
-    )
+    # spec_config = HybridDecodingConfig(
+    #     eagle_config=eagle_config,
+    #     ngram_config=ngram_config,
+    #     max_eagle_potential_drafts=2,
+    #     max_ngram_potential_drafts=32,
+    # )
+    # kv_cache_config = KvCacheConfig(enable_block_reuse=False)
 
-
-    kv_cache_config = KvCacheConfig(enable_block_reuse=False)
-
-    # llm = LLM(model='TinyLlama/TinyLlama-1.1B-Chat-v1.0', backend="pytorch", tensor_parallel_size=1)
     llm = LLM(
-        model="meta-llama/Meta-Llama-3-8B-Instruct",
+        model=MODEL_NAME,
         backend="pytorch",
         tensor_parallel_size=2,
-        # speculative_config=eagle_config,
-        speculative_config=spec_config,
-        kv_cache_config=kv_cache_config,
+        eagle_config=eagle_config,
+        # speculative_config=spec_config,
+        # kv_cache_config=kv_cache_config,
     )
     outputs = llm.generate(prompts, sampling_params)
 
@@ -46,7 +51,6 @@ def main():
         prompt = output.prompt
         generated_text = output.outputs[0].text
         print(f"[{i}] Prompt: {prompt!r}, Generated text: {generated_text!r}")
-
 
 if __name__ == '__main__':
     main()
